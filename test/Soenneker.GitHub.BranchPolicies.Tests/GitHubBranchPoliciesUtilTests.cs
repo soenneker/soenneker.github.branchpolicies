6ad7aa600506154cdf1d77c409368de471dfef9b
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:34:55 +0000

    xUnit v3

--- test/Soenneker.GitHub.BranchPolicies.Tests/GitHubBranchPoliciesUtilTests.cs
+++ test/Soenneker.GitHub.BranchPolicies.Tests/GitHubBranchPoliciesUtilTests.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Soenneker.GitHub.BranchPolicies.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-using Xunit.Abstractions;
+
 
 namespace Soenneker.GitHub.BranchPolicies.Tests;
 
